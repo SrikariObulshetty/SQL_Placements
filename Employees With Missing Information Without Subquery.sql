760d6b2dcb7538d781ccbca4a06c115d7243f9a8
Obulshetty Srikari <423164@student.nitandhra.ac.in>
2026-06-11 14:45:49 +0530

    Create Employees With Missing Information Without Subquery.sql

---
+++ Employees With Missing Information Without Subquery.sql
@@ -0,0 +1,19 @@
+# Write your MySQL query statement below.....
+# Full join
+SELECT e1.employee_id 
+FROM Employees as e1
+LEFT JOIN Salaries as s1 
+ON e1.employee_id = s1.employee_id
+WHERE s1.salary IS NULL
+UNION    
+SELECT s2.employee_id
+FROM Employees AS e2
+RIGHT JOIN Salaries AS s2
+ON e2.employee_id = s2.employee_id 
+WHERE e2.name IS NULL
+ORDER BY employee_id;   
+
+
+
+
+
